e947e69d57f282f6548e45b1f52a942d3c9a1d2d
Mark Elsinger <thesmokingamp@gmail.com>
2018-05-16 13:05:31 -0400

    Components and Dependencies Generated components. Installed bootstrap and peers.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GamesComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:"home",component:HomeComponent},
+      {path:"",redirectTo:"home"}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
