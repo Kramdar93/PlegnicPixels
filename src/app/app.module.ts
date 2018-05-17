689977f24fdcc168adb6b4a8395a6cd82d44a663
Mark Elsinger <thesmokingamp@gmail.com>
2018-05-16 22:07:01 -0400

    Added Components and Service Service will be through a perl cgi since that's what bluehost supports easiest. Lots of components generated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
 
 import { HomeComponent } from './home/home.component';
 import { GamesComponent } from './games/games.component';
 import { BlogComponent } from './blog/blog.component';
+import { NavComponent } from './nav/nav.component';
 
+import { ContentService } from "./services/content.service";
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GamesComponent,
-    BlogComponent
+    BlogComponent,
+    NavComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot([
       {path:"home",component:HomeComponent},
+      {path:"blog",component:BlogComponent},
+      {path:"games",component:GamesComponent},
       {path:"",redirectTo:"home"}
     ])
   ],
-  providers: [],
+  providers: [ContentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/content.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+
+@Injectable()
+export class ContentService {
+
+  CGI:string = "http://plegnicpixels.games/content.cgi"
+
+  constructor(private http:Http) { }
+
+  GetContent(type:"blog"|"games", id?:string){
+    return this.http.get(this.CGI,{params:{type:type, id:id}});
+  }
+
+}
